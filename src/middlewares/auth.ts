b3e03726fc505e60f437441ee4fe05f0ca815058
MicaelDev <micaelquiliao20@gmail.com>
2022-07-18 23:26:37 -0300

    Continuing studies with JWT authentications

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,11 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
+import { User } from '../models/User';
+import Jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
-export const Auth = {
-    private: (req: Request, res: Response, next: NextFunction) => {
+export const Auth = { 
+        private: async (req: Request, res: Response, next: NextFunction) => {
         let success = false;
         
-        // Fazer verificação de auth
+        if ( req.headers.authorization ) {
+            const [ authType, token ] = req.headers.authorization.split(' ');
+            if (authType === 'Bearer') {
+                try {
+                    const decoded = Jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+                    success = true;
+                } catch (error) {
+                }
+            }
+        }
 
         if (success) {
             next();
